60959cc9265089e28a2d75f56d83a232165555f8
Dopey59 <william.penet@epitech.eu>
2023-04-12 15:14:29 +0200

    [+] Day05

--- ex_03.php
+++ ex_03.php
@@ -4,6 +4,5 @@ function my_add_to_cookie($key , $value){
     setcookie($key, $valueModif, time()+3600*24, '/', '', false, false);
 }
 
-my_add_to_cookie("Max", "test");
-var_dump($_COOKIE);
+my_add_to_cookie("pseudo", "Max_");
 ?>
